b98d2b1ff59c725a05257be1767de1e4b3003861
Damian <damiancb97@gmail.com>
2024-12-29 02:08:37 +0100

    Primer commit, visual generado

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+import iconSteps from './assets/iconSteps.png'; 
+import iconKm from './assets/iconKm.png'; 
+import iconKcal from './assets/iconKcal.png'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Tus estadísticas diarias!</Text>
+      <Image source={iconSteps} style={{
+        width: 100,
+        height: 100,
+        resizeMode: 'center'
+      }} />
+      <Text>Pasos: </Text>
+      <Image source={iconKm} style={{
+        width: 100,
+        height: 100,
+        resizeMode: 'center'
+      }} />
+      <Text>Kms: </Text>
+      <Image source={iconKcal} style={{
+        width: 100,
+        height: 100,
+        resizeMode: 'center'
+      }} />
+      <Text>Kcal:: </Text>
       <StatusBar style="auto" />
+      <Button title='Pulsa aquí para acceder a tus estadísticas globales'/>
     </View>
   );
 }
