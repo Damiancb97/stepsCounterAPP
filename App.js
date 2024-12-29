50f11df7e737e1fb0e3d30160c6defbcdc9915b6
Damian <damiancb97@gmail.com>
2024-12-29 06:13:19 +0100

    Implementado constantes para kilometros y calories

--- App.js
+++ App.js
@@ -10,10 +10,14 @@ import iconKcal from './assets/iconKcal.png';
 export default function App() {
   const [steps, setSteps] = useState(0); // Estado para los pasos
   const [subscription, setSubscription] = useState(null); // Para gestionar el acelerómetro
+  const distance = steps * 0.04;
+  const calories = steps * 0.0008;
+
 
   const STEP_THRESHOLD = 1.2; // Umbral para detectar un paso
   let previousMagnitude = 0; // Magnitud previa del movimiento
 
+  // Función para manejar datos del acelerómetro
   const handleAccelerometerData = (data) => {
     const { x, y, z } = data;
     const magnitude = Math.sqrt(x * x + y * y + z * z); // Calcula la magnitud del movimiento
@@ -21,7 +25,6 @@ export default function App() {
     if (magnitude - previousMagnitude > STEP_THRESHOLD) {
       // Si supera el umbral, cuenta un paso
       setSteps((prevSteps) => prevSteps + 1);
-
     }
 
     previousMagnitude = magnitude; // Actualiza la magnitud previa
@@ -60,13 +63,13 @@ export default function App() {
         height: 100,
         resizeMode: 'center'
       }} />
-      <Text>Kms: </Text>
+      <Text>Kms: {distance.toFixed(2)} </Text>
       <Image source={iconKcal} style={{
         width: 100,
         height: 100,
         resizeMode: 'center'
       }} />
-      <Text>Kcal: </Text>
+      <Text>Kcal: {calories.toFixed(2)} </Text>
       <StatusBar style="auto" />
       <Button title='Pulsa aquí para acceder a tus estadísticas globales'/>
     </View>
